ca759db80fcfb45261ebdf93d066b83816d83e9f
Douglas Fabio da Cruz Junior <42014442+DouglasFabio@users.noreply.github.com>
2018-09-12 20:21:53 -0300

    Add files via upload

--- Incremento1/Codigo sql do banco bd_sggp.sql
+++ Incremento1/Codigo sql do banco bd_sggp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: 03-Set-2018 às 23:36
+-- Generation Time: 12-Set-2018 às 23:20
 -- Versão do servidor: 5.7.21
 -- PHP Version: 5.6.35
 
@@ -21,6 +21,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `bd_sggp`
 --
+CREATE DATABASE IF NOT EXISTS `bd_sggp` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `bd_sggp`;
 
 -- --------------------------------------------------------
 
@@ -40,6 +42,11 @@ CREATE TABLE IF NOT EXISTS `administradores` (
 --
 -- Extraindo dados da tabela `administradores`
 --
+
+INSERT INTO `administradores` (`Login`, `Email`, `Senha`, `Data`) VALUES
+('Admin', 'douglasfabio.ifsp@gmail.com', '123', '0000-00-00 00:00:00'),
+('testedata', 'douglasfabio.ifsp@gmail.com', '123', '0000-00-00 00:00:00');
+
 -- --------------------------------------------------------
 
 --
@@ -58,6 +65,13 @@ CREATE TABLE IF NOT EXISTS `lideres` (
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
+-- Extraindo dados da tabela `lideres`
+--
+
+INSERT INTO `lideres` (`Prontuario`, `Nome`, `Email`, `Senha`, `Data`, `verificaacesso_lider`) VALUES
+(1690094, 'Douglas Fabio', 'douglasfabio.ifsp@gmail.com', '0', '0000-00-00 00:00:00', 0),
+(1350081, 'Douglas Integrado', 'douglasfabio.integrado@gmail.com', '0', '0000-00-00 00:00:00', 0),
+(1690065, 'Douglas +1', 'douglas.mais1@gmail.com', '0', '2018-09-03 23:23:00', 0);
 
 -- --------------------------------------------------------
 
@@ -76,7 +90,7 @@ CREATE TABLE IF NOT EXISTS `verificaacesso` (
 --
 
 INSERT INTO `verificaacesso` (`acesso`) VALUES
-(1);
+(0);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
